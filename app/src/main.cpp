cbf0978d0615bc9a2823a0cd4dd23c3013704300
Sajan <sajantoor98@gmail.com>
2024-03-22 23:48:57 -0700

    Some more rough stuff

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,6 +1,9 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <messageHandler.hpp>
+#include <socket.hpp>
+#include <thread>
 #include <vector>
 
 #include "common/utils.hpp"
@@ -10,6 +13,7 @@
 #include "hal/relay.hpp"
 
 #define PASSWORD_FILE "password.txt"
+bool isStopped = false;
 
 // TODO: Add encryption to the password file, plain text is file for now
 void writeToFile(std::string filename, std::string content) {
@@ -39,6 +43,62 @@ std::string readFromFile(std::string filename) {
     return content;
 }
 
+// Input loop: wait for password input and handle it. This should be on
+// another thread.
+void inputLoop(std::string* password, Keypad* keypad, Relay* relay) {
+    while (!isStopped) {
+        std::cout << "Enter password: " << std::endl;
+
+        // TODO: Show input on the LCD display
+        keypad->startInput();
+        std::string input = keypad->getInput();
+
+        if (input == *password) {
+            std::cout << "Password correct, unlocking" << std::endl;
+            relay->open();
+            // TODO: Add a trigger to close the door...
+
+        } else {
+            std::cout << "Password incorrect, try again" << std::endl;
+
+            if (relay->isOpen()) {
+                relay->close();
+            }
+
+            // TODO: Could use BBG buzzer here to indicate incorrect password...
+
+            /**
+             * TODO: This could be done when multiple incorrect password
+             * attempts are made, or just after 1:
+             *
+             * We send a notification with the date and time of the attempt to
+             * the owner or owners using the web server (UDP message to web
+             * server).
+             *
+             * We also send webcam photo of the last 10-30 seconds.
+             */
+        }
+    }
+}
+
+void controlLoop() {
+    // get current time
+
+    while (!isStopped) {
+        /**
+         * Send the current state of the door and the last time it was
+            opened show any notifications that have been sent show live feed
+        from webcam
+        */
+
+        if (MotionSensor::isMotionDetected()) {
+            // start recording
+            // send notification to user after certain amount of time to
+            // debounce
+        }
+    }
+}
+
 int main(void) {
     // Init hardware
     Relay relay;
@@ -62,60 +122,13 @@ int main(void) {
         writeToFile(PASSWORD_FILE, password);
     }
 
-    // Input loop: wait for password input and handle it. This should be on
-    // another thread.
-    while (true) {
-        std::cout << "Enter password: " << std::endl;
+    Socket socket;
+    MessageHandler messageHandler(&socket);
 
-        // TODO: Show input on the LCD display
-        keypad.startInput();
-        std::string input = keypad.getInput();
+    std::thread controlLoopThread = std::thread(controlLoop);
 
-        if (input == password) {
-            std::cout << "Password correct, unlocking" << std::endl;
-            relay.open();
-            // TODO: Add a trigger to close the door...
-
-        } else {
-            std::cout << "Password incorrect, try again" << std::endl;
-
-            if (relay.isOpen()) {
-                relay.close();
-            }
-
-            // TODO: Could use BBG buzzer here to indicate incorrect password...
-
-            /**
-             * TODO: This could be done when multiple incorrect password
-             * attempts are made, or just after 1:
-             *
-             * We send a notification with the date and time of the attempt to
-             * the owner or owners using the web server (UDP message to web
-             * server).
-             *
-             * We also send webcam photo of the last 10-30 seconds.
-             */
-        }
-    }
-
-    // This is the main application loop:
-    while (true) {
-        /**
-         * Send the current state of the door and the last time it was
-            opened show any notifications that have been sent show live feed
-        from webcam
-
-
-        * Check motion sensor for any movement and start recording if there
-            is any, until there is no movement.
-
-
-        * Check for any messages from the web server, ie lock or unlock door
-            events and act accordingly. Check for change password events
-
-        * Show a message on the LCD? "Door is locked" or "Door is unlocked", etc
-        */
-    }
+    inputLoop(&password, &keypad, &relay);
+    controlLoopThread.join();
 
     return 0;
 }
