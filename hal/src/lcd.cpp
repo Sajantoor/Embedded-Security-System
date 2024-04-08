cf110a9c3609d660ff33375d1ff13579148bf1cf
adpop <adpop@debian-11.8.0-beaglebone.myguest.virtualbox.org>
2024-04-07 18:48:26 -0700

    renamed variables

--- hal/src/lcd.cpp
+++ hal/src/lcd.cpp
@@ -21,34 +21,7 @@ LCD::LCD() {
     GPIO gpio = GPIO();
     this->gpio = gpio;
 
-    gpio.exportPin(LcdGpioPins::D4);
-    gpio.exportPin(LcdGpioPins::D5);
-    gpio.exportPin(LcdGpioPins::D6);
-    gpio.exportPin(LcdGpioPins::D7);
-    gpio.exportPin(LcdGpioPins::RS);
-    gpio.exportPin(LcdGpioPins::E);
-
-    gpio.configPin(P9, 27, "gpio");
-    gpio.configPin(P9, 15, "gpio");
-    gpio.configPin(P8, 7, "gpio");
-    gpio.configPin(P8, 9, "gpio");
-    gpio.configPin(P8, 10, "gpio");
-    gpio.configPin(P8, 8, "gpio");
-
-    gpio.setPinDirection(LcdGpioPins::D4, "out");
-    gpio.setPinDirection(LcdGpioPins::D5, "out");
-    gpio.setPinDirection(LcdGpioPins::D6, "out");
-    gpio.setPinDirection(LcdGpioPins::D7, "out");
-    gpio.setPinDirection(LcdGpioPins::RS, "out");
-    gpio.setPinDirection(LcdGpioPins::E, "out");
-
-    gpio.setPinValue(LcdGpioPins::D4, 0);
-    gpio.setPinValue(LcdGpioPins::D5, 0);
-    gpio.setPinValue(LcdGpioPins::D6, 0);
-    gpio.setPinValue(LcdGpioPins::D7, 0);
-    gpio.setPinValue(LcdGpioPins::RS, 0);
-    gpio.setPinValue(LcdGpioPins::E, 0);
-
+    initGpioForLCD();
     initLCD();
 
     functionSet(0, 1, 0);  // Function set: 2 line display, 5x8 dot char font
@@ -77,6 +50,36 @@ void LCD::stop(void) {
     clearDisplay();
 }
 
+void LCD::initGpioForLCD() {
+    gpio.exportPin(LcdGpioPins::D4);
+    gpio.exportPin(LcdGpioPins::D5);
+    gpio.exportPin(LcdGpioPins::D6);
+    gpio.exportPin(LcdGpioPins::D7);
+    gpio.exportPin(LcdGpioPins::RS);
+    gpio.exportPin(LcdGpioPins::E);
+
+    gpio.configPin(P9, 27, "gpio");
+    gpio.configPin(P9, 15, "gpio");
+    gpio.configPin(P8, 7, "gpio");
+    gpio.configPin(P8, 9, "gpio");
+    gpio.configPin(P8, 10, "gpio");
+    gpio.configPin(P8, 8, "gpio");
+
+    gpio.setPinDirection(LcdGpioPins::D4, "out");
+    gpio.setPinDirection(LcdGpioPins::D5, "out");
+    gpio.setPinDirection(LcdGpioPins::D6, "out");
+    gpio.setPinDirection(LcdGpioPins::D7, "out");
+    gpio.setPinDirection(LcdGpioPins::RS, "out");
+    gpio.setPinDirection(LcdGpioPins::E, "out");
+
+    gpio.setPinValue(LcdGpioPins::D4, 0);
+    gpio.setPinValue(LcdGpioPins::D5, 0);
+    gpio.setPinValue(LcdGpioPins::D6, 0);
+    gpio.setPinValue(LcdGpioPins::D7, 0);
+    gpio.setPinValue(LcdGpioPins::RS, 0);
+    gpio.setPinValue(LcdGpioPins::E, 0);
+}
+
 void LCD::initLCD() {
     // The first 3 function set calls lets the lcd know to reset and
     // reinitialize on the 4th function set
@@ -96,13 +99,10 @@ void LCD::initLCD() {
 void LCD::write4bits(uint8_t value) {
     uint8_t bit = value & 0x01;
     gpio.setPinValue(LcdGpioPins::D4, bit);
-
     bit = (value >> 1) & 0x01;
     gpio.setPinValue(LcdGpioPins::D5, bit);
-
     bit = (value >> 2) & 0x01;
     gpio.setPinValue(LcdGpioPins::D6, bit);
-
     bit = (value >> 3) & 0x01;
     gpio.setPinValue(LcdGpioPins::D7, bit);
 
@@ -147,63 +147,67 @@ void LCD::returnHome() {
 
 void LCD::entryModeSet(bool cursorDir, uint8_t displayShift) {
     gpio.setPinValue(LcdGpioPins::RS, 0);
-    uint8_t id = cursorDir << 1;
-    uint8_t val = 0x4 + id + displayShift;
+    uint8_t cursorDirInt = cursorDir << 1;
+    uint8_t lcdInstruction = (0x4 + cursorDirInt + displayShift) & 0xF;
     write4bits(0x0);
-    write4bits(val & 0xF);
+    write4bits(lcdInstruction);
     sleepForNs(64000);
 }
 
 void LCD::displayControl(bool dispOn, bool cursorOn, bool blinkOn) {
     gpio.setPinValue(LcdGpioPins::RS, 0);
-    uint8_t d = dispOn << 2;
-    uint8_t c = cursorOn << 1;
-    uint8_t val = 0x8 + d + c + blinkOn;
+    uint8_t dispOnInt = dispOn << 2;
+    uint8_t cursonOnInt = cursorOn << 1;
+    uint8_t lcdInstruction = (0x8 + dispOnInt + cursonOnInt + blinkOn) & 0xF;
     write4bits(0x0);
-    write4bits(val & 0xF);
+    write4bits(lcdInstruction);
     sleepForNs(64000);
 }
 
 void LCD::cursorOrDisplayShift(bool shiftOrCursor, bool rightOrLeft) {
     gpio.setPinValue(LcdGpioPins::RS, 0);
-    uint8_t sc = shiftOrCursor << 3;
-    uint8_t rl = rightOrLeft << 2;
-    uint8_t val = sc + rl;
+    uint8_t shiftOrCursorInt = shiftOrCursor << 3;
+    uint8_t rightOrLeftInt = rightOrLeft << 2;
+    uint8_t lcdInstruction = (shiftOrCursorInt + rightOrLeftInt) & 0xF;
     write4bits(0x1);
-    write4bits(val & 0xF);
+    write4bits(lcdInstruction);
     sleepForNs(64000);
 }
 
 void LCD::functionSet(bool dispMode, bool numOfLines, bool fontSize) {
     gpio.setPinValue(LcdGpioPins::RS, 0);
-    uint8_t upperVal = 0x2 + dispMode;
-    uint8_t n = numOfLines << 3;
-    uint8_t f = fontSize << 2;
-    uint8_t lowerVal = (n + f);
-    write4bits(upperVal);
-    write4bits(lowerVal & 0xF);
+    uint8_t lcdInstructionUpperHalf = 0x2 + dispMode;
+    uint8_t numOfLinesInt = numOfLines << 3;
+    uint8_t fontSizeInt = fontSize << 2;
+    uint8_t lcdInstructionLowerHalf = (numOfLinesInt + fontSizeInt) & 0xF;
+    write4bits(lcdInstructionUpperHalf);
+    write4bits(lcdInstructionLowerHalf);
     sleepForNs(64000);
 }
 
 void LCD::setCgramAddress(uint8_t addr) {
     gpio.setPinValue(LcdGpioPins::RS, 0);
-    uint8_t upperVal = 0x4 + (addr >> 4);
-    uint8_t lowerVal = addr;
-    write4bits(upperVal);
-    write4bits(lowerVal & 0xF);
+    uint8_t lcdInstructionUpperHalf = 0x4 + (addr >> 4);
+    uint8_t lcdInstructionLowerHalf = addr & 0xF;
+    write4bits(lcdInstructionUpperHalf);
+    write4bits(lcdInstructionLowerHalf);
     sleepForNs(64000);
 }
 
 void LCD::setDdramAddress(uint8_t addr) {
     gpio.setPinValue(LcdGpioPins::RS, 0);
-    write4bits(0x8 + (addr >> 4));
-    write4bits(addr & 0xF);
+    uint8_t lcdInstructionUpperHalf = 0x8 + (addr >> 4);
+    uint8_t lcdInstructionLowerHalf = addr & 0xF;
+    write4bits(lcdInstructionUpperHalf);
+    write4bits(lcdInstructionLowerHalf);
     sleepForNs(64000);
 }
 
-void LCD::writeCharacter(char c) {
-    write4bits(c >> 4);
-    write4bits(c & 0xF);
+void LCD::writeCharacter(char character) {
+    uint8_t lcdInstructionUpperHalf = character >> 4;
+    uint8_t lcdInstructionLowerHalf = character & 0xF;
+    write4bits(lcdInstructionUpperHalf);
+    write4bits(lcdInstructionLowerHalf);
     sleepForNs(64000);
 }
 
