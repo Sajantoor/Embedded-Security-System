65d3d216322a6198e6618424b3b81ad784387c3f
Sajan <sajantoor98@gmail.com>
2024-04-15 12:13:42 -0700

    Update sample program

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,25 +1,16 @@
-#include <iostream>
-#include <string>
-#include <vector>
 
-#include "common/utils.hpp"
 #include "hal/webcam.hpp"
-#include "main.hpp"
-#include "notifier.hpp"
 #include "socket.hpp"
 
 int main(void) {
     Socket socket;
     Webcam webcam(&socket);
-    Notifier notifier(&socket);
-    
+
     webcam.startStream();
 
-    for (int i = 0; i < 2; i++){
-        notifier.notify(DOOR_CLOSED);
-        sleepForMs(1000);
-    }
+    socket.sendToWebServer("Hello from the BeagleBone!");
 
+    socket.closeSocket();
     webcam.stopStream();
     return 0;
 }
--- web-app/server/index.js
+++ web-app/server/index.js
@@ -1,39 +1,24 @@
-const { Server } = require('socket.io');
 const dgram = require('dgram');
 const axios = require('axios');
-require('dotenv').config();
 const child = require("child_process");
+require('dotenv').config();
 
 const DISCORD_WEBHOOK = process.env.DISCORD_WEBHOOK;
 if (!DISCORD_WEBHOOK) {
   throw new Error('No environment variable DISCORD_WEBHOOK found.');
 }
 
-const webSocketServer = new Server({
-  cors: {
-    origin: 'http://localhost:3000',
-  },
-});
-
-const port = 4000;
-webSocketServer.listen(port);
-console.log(`WebSocket server listening on port ${port}`);
-
-const DISCORD_EMBED_COLOR = 5814783;
-
 const UDP_SERVER_ADDRESS = '192.168.6.1';
 const UDP_SERVER_PORT = 7070;
-
-const UDP_BBG_ADDRESS = '192.168.6.2';
-const UDP_BBG_PORT = 12345;
-const UDP_BBG_STREAMING_PORT = 1234;
 
 const udpServer = dgram.createSocket('udp4');
 udpServer.bind(UDP_SERVER_PORT, UDP_SERVER_ADDRESS);
 
+const UDP_BBG_ADDRESS = '192.168.6.2';
+const UDP_BBG_STREAMING_PORT = 1234;
+
 let ffmpegProcess = startFFMpegProcess();
 let frame = null;
-let currentClientSocket = null;
 
 // https://stackoverflow.com/questions/16245767/creating-a-blob-from-a-base64-string-in-javascript
 const b64toBlob = (b64Data, contentType = '', sliceSize = 512) => {
@@ -57,6 +42,8 @@ const b64toBlob = (b64Data, contentType = '', sliceSize = 512) => {
 };
 
 // Send message to Discord through Webhook
+const DISCORD_EMBED_COLOR = 5814783;
+
 const sendDiscordMessage = (message, base64String = '') => {
   const image = base64String ? b64toBlob(base64String.replace(/^data:image\/[a-z]+;base64,/, ''), 'image/jpeg') : null;
   const data = {
@@ -67,8 +54,8 @@ const sendDiscordMessage = (message, base64String = '') => {
         color: DISCORD_EMBED_COLOR,
         image: image
           ? {
-              url: `attachment://image.jpeg`,
-            }
+            url: `attachment://image.jpeg`,
+          }
           : null,
       },
     ],
@@ -83,66 +70,10 @@ const sendDiscordMessage = (message, base64String = '') => {
   axios.post(DISCORD_WEBHOOK, formData, { headers: headers }).catch((error) => console.error(error));
 };
 
-// Send messages to discord, does not require a socket connection
 udpServer.on('message', (msg) => {
-  const parsedMessage = parseMessage(msg.toString());
-
-  if (!parsedMessage) {
-    return;
-  }
-
-  const command = parsedMessage.command;
-
-  if (command === COMMANDS.HEARTBEAT) {
-    return;
-  }
-
-  const message = parsedMessage.message;
-  const timestamp = convertEpochTimeToTimestamp(parsedMessage.epochTime);
-  const discordMessage = `**${timestamp}**: ${message}`;
-  sendDiscordMessage(discordMessage, getCurrentFrame());
+  sendDiscordMessage(msg.toString(), getCurrentFrame());
 });
 
-webSocketServer.on('connection', (socket) => {
-  console.log("Connected");
-  currentClientSocket = socket;
-
-  // Send messages to the connected client
-  udpServer.on('message', (msg) => {
-    console.log('Received message from UDP server: ', msg.toString());
-    const parsedMessage = parseMessage(msg.toString(), socket);
-
-    if (!parsedMessage) {
-      return;
-    }
-
-    const command = parsedMessage.command;
-    if (command === COMMANDS.HEARTBEAT) {
-      socket.emit('heartbeat', parsedMessage.message);
-      return;
-    }
-
-    socket.emit('event', {
-      message: parsedMessage.message,
-      epochTime: parsedMessage.epochTime,
-      image: getCurrentFrame()
-    });
-  });
-
-  socket.on("connect", () => {
-    console.log("user connected");
-  });
-
-  socket.on("disconnect", () => {
-    currentClientSocket = null;
-    console.log("user disconnected");
-  });
-
-  socket.on('message', (message) => {
-    console.log('Received message: ', message);
-    udpServer.send(message, UDP_BBG_PORT, UDP_BBG_ADDRESS);
-  });
-});
 
 function startFFMpegProcess() {
   console.log("ffmpeg process started");
@@ -165,7 +96,6 @@ function startFFMpegProcess() {
 
   return child.spawn("ffmpeg", ffmpegArgs);
 }
-
 
 ffmpegProcess.on("connect", function () {
   console.log("ffmpeg connected");
@@ -191,80 +121,8 @@ ffmpegProcess.stderr.on("data", function (data) {
 
 ffmpegProcess.stdout.on("data", function (data) {
   frame = Buffer.from(data).toString("base64"); //convert raw data to string
-
-  if (currentClientSocket) {
-    currentClientSocket.emit("canvas", frame);
-  }
-
 });
 
 function getCurrentFrame() {
-	return frame ? "data:image/jpeg;base64," + frame : "";
-}
-
-const COMMANDS = {
-  DOOR_OPEN: 'doorOpen',
-  DOOR_CLOSED: 'doorClosed',
-  FAILED_PASSWORD: 'failedPassword',
-  MOTION_DETECTED: 'motionDetected',
-  PASSWORD_CHANGED: 'passwordChanged',
-  PASSWORD_SET: 'passwordSet',
-  PASSWORD_CHANGE_FAILED: 'passwordChangeFailed',
-  DISPLAY_MESSAGE_SET: 'displayMessageSet',
-  DISPLAY_MESSAGE_FAILED: 'displayMessageFailed',
-  HEARTBEAT: 'heartbeat',
-};
-
-function convertEpochTimeToTimestamp(epochTime) {
-  const date = new Date(0);
-  date.setUTCSeconds(epochTime);
-  return date.toLocaleString("en-US", { timeZone: "America/Vancouver" });
-}
-
-function parseMessage(message, socket) {
-  const tokens = message.split(' ');
-  const command = tokens[0];
-  const epochTime = parseInt(tokens[1]);
-
-  let messageSent = '';
-
-  switch (command) {
-    case COMMANDS.DOOR_OPEN:
-      message = 'Door Opened';
-      break;
-    case COMMANDS.DOOR_CLOSED:
-      message = 'Door Closed';
-      break;
-    case COMMANDS.FAILED_PASSWORD:
-      messageSent = tokens.slice(2).join(' ');
-      message = `Failed Password: ${messageSent}`;
-      break;
-    case COMMANDS.PASSWORD_CHANGED:
-      message = 'Password Changed';
-      break;
-    case COMMANDS.PASSWORD_SET:
-      message = 'Password Set';
-      break;
-    case COMMANDS.PASSWORD_CHANGE_FAILED:
-      message = 'Password Change Failed';
-      break;
-    case COMMANDS.DISPLAY_MESSAGE_SET:
-      message = 'Display Message Set'
-      break;
-    case COMMANDS.MOTION_DETECTED:
-      message = 'Motion Detected'
-      break;
-    case COMMANDS.DISPLAY_MESSAGE_FAILED:
-      messageSent = tokens.slice(2).join(' ');
-      message += `Display Message Failed: ${messageSent}`;
-      break;
-    case COMMANDS.HEARTBEAT:
-      // do nothing
-      break;
-    default:
-      console.error(`Unknown command: ${command} and message: ${message}`);
-      return;
-  }
-
-  return { message, epochTime, command };
+  return frame ? "data:image/jpeg;base64," + frame : "";
 }
